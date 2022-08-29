fef4f4f7134c002268e147db34d316424b5243a3
acmoxx <111687088+acmoxx@users.noreply.github.com>
2022-08-29 18:41:12 +0300

    Add files via upload

---
+++ kyrsovaya/kyrsovaya/kyrsovaya/kyrsovaya/kyrsovaya/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include "electrician.h"
+
+int main()
+{
+    try
+    {
+        Object object("object", 2, 3);
+        Object object1("object1", 3, 3);
+
+        Electrician electrician("Nndnd fhghg ghhgh", 18, 170, 60, "man",2, 3, &object);
+        electrician.info();
+        electrician.setObject(&object1);
+        electrician.info();
+    }
+    catch (const std::exception& ex) { std::cout << "exception:" << ex.what(); }
+    return 0;
+}
