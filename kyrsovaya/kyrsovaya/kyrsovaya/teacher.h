262d6f415f3bfac8bee52be9e4678c1804c8d3b6
acmoxx <111687088+acmoxx@users.noreply.github.com>
2022-09-01 20:30:21 +0300

    Update teacher.h

--- kyrsovaya/kyrsovaya/kyrsovaya/teacher.h
+++ kyrsovaya/kyrsovaya/kyrsovaya/teacher.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "person.h"
+
+enum class Departament { internationalRelations, politicalScience, historyAndWorldPolitics};
+enum class Degree { graduateStudent, candidate, doctor};
+
+class Teacher:public Person
+{
+public:
+    Teacher(std::string fullName, int age, int height, int weight, std::string gender,
+        std::string university, std::string subject, Departament dt, int experience, Degree de, int salary, int amountOfWork);
+    Departament getDepartament() { return departament; }
+    Degree getDegree() { return degree; }
+    int payroll();
+    void extent();
+    void info() override;
+private:
+	std::string university;
+	std::string subject;
+    Departament departament;
+    int experience;
+    Degree degree;
+    int salary;
+    int amountOfWork;
+};
